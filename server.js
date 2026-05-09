a749ff394f523284547ae4af3945bd4369a64a57
Ganesh <ganeshrbali@gmail.com>
2026-05-09 20:27:24 +0530

    Fixed Gmail SMTP connection

--- server.js
+++ server.js
@@ -8,13 +8,14 @@ const nodemailer = require("nodemailer");
 const app = express();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
-pass: process.env.EMAIL_PASS,
+    pass: process.env.EMAIL_PASS,
   },
 });
-
 app.use(cors());
 app.use(express.json());
 
